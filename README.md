dab423e6bb677ec76ea18aa35129300f9cd0c4b0
Colin Coleman <colin.coleman@kontrollsmart.com>
2018-03-06 12:45:48 +0100

    added default AWS_REGION updated README

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # sonarqube-ssm
 Sonarqube Docker with secrets pulled from AWS SSM Parameter Store
+
+To use this docker container the values for SONARQUBE_JDBC_USERNAME, SONARQUBE_JDBC_PASSWORD and SONARQUBE_JDBC_PASSWORD should be set in AWS SSM Parameter store.
+
+This docker container expects the following environment variables to be set with the names used in AWS SSM to reference the real values for the JDBC connection.
+
+ - SSM_PARAMETER_NAME_SONARQUBE_JDBC_USERNAME
+ - SSM_PARAMETER_NAME_SONARQUBE_JDBC_PASSWORD
+ - SSM_PARAMETER_NAME_SONARQUBE_JDBC_PASSWORD
+ - AWS_REGION (Optional - defaults to eu-west-1 if not set)
--- docker/start-with-params.sh
+++ docker/start-with-params.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-export SONARQUBE_JDBC_USERNAME=`aws ssm get-parameters --names $SSM_PARAMETER_NAME_SONARQUBE_JDBC_USERNAME --with-decryption --region eu-west-1 |jq -r .Parameters[].Value` \
-export SONARQUBE_JDBC_PASSWORD=`aws ssm get-parameters --names $SSM_PARAMETER_NAME_SONARQUBE_JDBC_PASSWORD --with-decryption --region eu-west-1 |jq -r .Parameters[].Value` \
-export SONARQUBE_JDBC_PASSWORD=`aws ssm get-parameters --names $SSM_PARAMETER_NAME_SONARQUBE_JDBC_PASSWORD --with-decryption --region eu-west-1 |jq -r .Parameters[].Value`
+if [ -z "$AWS_REGION"];then
+AWS_REGION="eu-west-1"
+fi
+export SONARQUBE_JDBC_USERNAME=`aws ssm get-parameters --names $SSM_PARAMETER_NAME_SONARQUBE_JDBC_USERNAME --with-decryption --region $AWS_REGION |jq -r .Parameters[].Value` \
+export SONARQUBE_JDBC_PASSWORD=`aws ssm get-parameters --names $SSM_PARAMETER_NAME_SONARQUBE_JDBC_PASSWORD --with-decryption --region $AWS_REGION |jq -r .Parameters[].Value` \
+export SONARQUBE_JDBC_PASSWORD=`aws ssm get-parameters --names $SSM_PARAMETER_NAME_SONARQUBE_JDBC_PASSWORD --with-decryption --region $AWS_REGION |jq -r .Parameters[].Value`
 ./bin/run.sh
