4671fa1211fa2f20b514ebd6f835fd7444eb174e
Colin Coleman <colin.coleman@kontrollsmart.com>
2018-03-06 13:40:57 +0100

    added correct prefix to url paramater fixed copy paste error with url parameter

--- docker/start-with-params.sh
+++ docker/start-with-params.sh
@@ -4,5 +4,5 @@ AWS_REGION="eu-west-1"
 fi
 export SONARQUBE_JDBC_USERNAME=`aws ssm get-parameters --names $SSM_PARAMETER_NAME_SONARQUBE_JDBC_USERNAME --with-decryption --region $AWS_REGION |jq -r .Parameters[].Value` \
 export SONARQUBE_JDBC_PASSWORD=`aws ssm get-parameters --names $SSM_PARAMETER_NAME_SONARQUBE_JDBC_PASSWORD --with-decryption --region $AWS_REGION |jq -r .Parameters[].Value` \
-export SONARQUBE_JDBC_PASSWORD=`aws ssm get-parameters --names $SSM_PARAMETER_NAME_SONARQUBE_JDBC_PASSWORD --with-decryption --region $AWS_REGION |jq -r .Parameters[].Value`
+export SONARQUBE_JDBC_URL=`aws ssm get-parameters --names $SSM_PARAMETER_NAME_SONARQUBE_JDBC_URL --with-decryption --region $AWS_REGION |jq -r .Parameters[].Value`
 ./bin/run.sh
